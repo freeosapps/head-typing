776c5ea509109decdf7234d1a143eb984b0f3415
Bruno <freeosapps@gmail.com>
2017-12-30 15:56:43 -0200

    Correção de letras minúsculas

--- arvore-de-palavras.js
+++ arvore-de-palavras.js
@@ -3,7 +3,7 @@ class ArvoreDePalavras {
         let letras = new Set();
         $($('#wordlist')[0].import).find('body').text().match(new RegExp(prefixo + '.*\n', 'gi'))
         .forEach(palavra => {
-           letras.add(palavra.charAt(prefixo.length));
+           letras.add(palavra.charAt(prefixo.length).toUpperCase());
         });
         return [...letras];
     }
--- testes/arvore-de-palavras.js
+++ testes/arvore-de-palavras.js
@@ -18,16 +18,16 @@ describe('ArvoreDePalavras', () => {
             expect(arvoreDePalavras.quaisAsProximasLetras('').length).toBe(6);
         });
 
-        it('possui raiz com a letra "d" como primeira letra', () => {
-            expect(arvoreDePalavras.quaisAsProximasLetras('')[0]).toBe('d');
+        it('possui raiz com a letra "D" como primeira letra', () => {
+            expect(arvoreDePalavras.quaisAsProximasLetras('')[0]).toBe('D');
         });
 
-        it('possui raiz com a letra "e" como segunda letra', () => {
-            expect(arvoreDePalavras.quaisAsProximasLetras('')[1]).toBe('e');
+        it('possui raiz com a letra "E" como segunda letra', () => {
+            expect(arvoreDePalavras.quaisAsProximasLetras('')[1]).toBe('E');
         });
 
-        it('possui raiz com a letra "a" como terceira letra', () => {
-            expect(arvoreDePalavras.quaisAsProximasLetras('')[2]).toBe('a');
+        it('possui raiz com a letra "A" como terceira letra', () => {
+            expect(arvoreDePalavras.quaisAsProximasLetras('')[2]).toBe('A');
         });
     });
     describe('com 5 palavras e sem prefixo', () => {
@@ -41,16 +41,16 @@ describe('ArvoreDePalavras', () => {
             $('#wordlist').prop('id', 'wordlist-b');
         });
 
-        it('possui raiz com a letra "q" como primeira letra', () => {
-            expect(arvoreDePalavras.quaisAsProximasLetras('')[0]).toBe('q');
+        it('possui raiz com a letra "Q" como primeira letra', () => {
+            expect(arvoreDePalavras.quaisAsProximasLetras('')[0]).toBe('Q');
         });
 
-        it('possui raiz com a letra "u" como segunda letra', () => {
-            expect(arvoreDePalavras.quaisAsProximasLetras('')[1]).toBe('u');
+        it('possui raiz com a letra "U" como segunda letra', () => {
+            expect(arvoreDePalavras.quaisAsProximasLetras('')[1]).toBe('U');
         });
 
-        it('possui raiz com a letra "o" como terceira letra', () => {
-            expect(arvoreDePalavras.quaisAsProximasLetras('')[2]).toBe('o');
+        it('possui raiz com a letra "O" como terceira letra', () => {
+            expect(arvoreDePalavras.quaisAsProximasLetras('')[2]).toBe('O');
         });
     });
     describe('com 3 palavras e com prefixo', () => {
@@ -64,16 +64,16 @@ describe('ArvoreDePalavras', () => {
             $('#wordlist').prop('id', 'wordlist-c');
         });
 
-        it('possui raiz com a palavra "co"', () => {
-            expect(arvoreDePalavras.quaisAsProximasLetras('c')[0]).toBe('o');
+        it('possui raiz com a palavra "CO"', () => {
+            expect(arvoreDePalavras.quaisAsProximasLetras('C')[0]).toBe('O');
         });
 
-        it('possui raiz com a palavra "um"', () => {
-            expect(arvoreDePalavras.quaisAsProximasLetras('u')[0]).toBe('m');
+        it('possui raiz com a palavra "UM"', () => {
+            expect(arvoreDePalavras.quaisAsProximasLetras('U')[0]).toBe('M');
         });
 
-        it('possui raiz com a palavra "com"', () => {
-            expect(arvoreDePalavras.quaisAsProximasLetras('co')[0]).toBe('m');
+        it('possui raiz com a palavra "COM"', () => {
+            expect(arvoreDePalavras.quaisAsProximasLetras('CO')[0]).toBe('M');
         });
     });
 });

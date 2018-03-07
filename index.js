1065e314de1ad467be720557cc1d5afdd9a176da
Bruno Ramos <brunomrsn@gmail.com>
2018-03-07 17:50:49 -0300

    Melhoria no leiaute

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let placa = new Placa();
 let relogio = new Relogio();
 let anotacao = new Anotacao();
 let interlocutor = new Interlocutor(placa, relogio, anotacao);
-$(() => {  
+$(() => {
     interlocutor.prepararParaMostrar(document.body);
 });
